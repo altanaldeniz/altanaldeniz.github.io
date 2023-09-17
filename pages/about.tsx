b959a310d932eb2533df991c477e3558d3224b17
Altan Aldeniz <altanaldeniz@gmail.com>
2023-09-17 21:47:58 +0300

    Update about.tsx

--- pages/about.tsx
+++ pages/about.tsx
@@ -2,31 +2,7 @@ const About = () => {
   return (
     <div className="flex mt-5 mb-5 ">
       <article className="text-xl leading-8">
-        <p>
-          I was born in 2000 and from a young age, I was fascinated by
-          computers. Even as a kindergartener, I couldn't take my eyes off the
-          screen of the Toshiba laptop that my father brought home.
-        </p>
-        <p className="mt-2">
-          Although we didn't have internet at home until I was in sixth grade, I
-          continued to tinker with the computer, though it was usually finishing
-          the Warcraft III campaign numerous times (which perhaps explains my
-          success at MOBA games).
-        </p>
-        <p className="mt-2">
-          As I grew older, my interest in computers only deepened. When I
-          enrolled in a computer engineering program at university, I quickly
-          became enthusiastic with coding and web development in particular.
-          Since then, I've been pursuing my passion for web development with
-          enthusiasm and a commitment to constantly learning and growing as a
-          developer.
-        </p>
-        <p className="mt-2">
-          During my free time, I like to explore the latest technologies and
-          trends in the field of web development, as well as continuously
-          improving my skills with existing ones. I also enjoy playing video
-          games for brainshifting and relaxing.
-        </p>
+       .
       </article>
     </div>
   );
